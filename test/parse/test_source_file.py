466afb2e5c2a48b60c4a5404e61aa7b6f332319b
malyvsen <5940672+malyvsen@users.noreply.github.com>
2021-04-01 19:15:39 +0200

    test: parsing file with multiple functions

--- test/parse/test_source_file.py
+++ test/parse/test_source_file.py
@@ -26,3 +26,40 @@ def normalize(vector):
             )
         },
     )
+
+
+def test_multiple_functions():
+    code = """
+import numpy as np
+
+def normalize(vector):
+    return vector / length(vector)
+
+def length(vector):
+    return np.sqrt(np.sum(vector ** 2))
+"""
+    assert SourceFile.from_code(code) == SourceFile(
+        imports=Imports(modules={Imports.Alias(original="numpy", renamed="np")}),
+        functions={
+            Function(
+                name="normalize",
+                docstring=None,
+                arguments=("vector",),
+                returns=frozenset(),
+                dependencies=frozenset(),
+                num_operations=3,
+                first_line=3,
+                last_line=5,
+            ),
+            Function(
+                name="length",
+                docstring=None,
+                arguments=("vector",),
+                returns=frozenset(),
+                dependencies=frozenset({"numpy"}),
+                num_operations=4,
+                first_line=6,
+                last_line=8,
+            ),
+        },
+    )
